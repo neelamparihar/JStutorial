97681383dc3da4b38104e8ce7e88d66beb8cfa6d
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-23 05:51:49 +0000

    math

---
+++ 01_basics/06_math.js
@@ -0,0 +1,8 @@
+// console.log(Math.max(2,86,64,65));
+// console.log(Math.min(2,86,64,65));
+// console.log(Math.floor(2.7853652));
+// console.log(Math.ceil(2.867367));
+
+let min = 1;
+let max = 6;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
