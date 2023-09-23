2e265dd458fcabb7a108086f5256538c405720d3
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-23 01:46:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all JS code as newer version
+
+let name = "Neelam"
+let age = 18
+let isLoggedIn = false
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//Object
+console.log(typeof  null);
+console.log(typeof undefined);
