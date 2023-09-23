99e523e711ec30423fec4ac4d29ff966786307f0
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-23 04:48:56 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,13 @@
+let score = 33;
+console.log(typeof score);
+
+let score1 = "33";
+console.log(typeof score1);
+
+let score2 = Number(score1);
+console.log(typeof score2);
+
+let score3 = "33abc"
+let score4 = Number(score3);
+console.log(typeof score4);
+console.log(score4);
