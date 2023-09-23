08d4926d7712461ceb19ce05404b57eef4d1e7e7
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-23 07:13:09 +0000

    array

---
+++ 01_basics/08_array.js
@@ -0,0 +1,25 @@
+const arr= [0,1,2,3,4,5,6,7,8,9];
+// console.log(arr[5]);
+// console.log(arr);
+
+// arr.push(10);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(11);
+// console.log(arr);
+
+const arr2 = [874,34,3,434];
+const arr3 = arr.concat(arr2);
+console.log(arr3);
+
+//spread operator
+const arr33 = ["heyy", "I", "am", "Neelam"];
+const arr4 = [...arr,...arr2,...arr33];
+console.log(arr4);
+
+
+
+
