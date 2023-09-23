538f712002b500ed88a36531df7d1890ca4a8a84
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-23 01:49:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const id = 12345;
+var emailId = "neelam@gmail.com";
+let passwordId = "secret"
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([id,emailId,passwordId])
