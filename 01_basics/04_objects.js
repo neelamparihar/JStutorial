ce1bccfae25fe50bf4a52f1430efd4c086855752
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-23 05:37:18 +0000

    commit object and string file

---
+++ 01_basics/04_objects.js
@@ -0,0 +1,12 @@
+let userOne = {
+    email : "neelam@gmail.com",
+    id : "12345"
+}
+
+let userTwo= userOne;
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+//primitives datatype => stack
+//Non-primitives datatype => heap
---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+let name = "Neelam";
+let age = 20;
+
+console.log(`My name is ${name} and my age is ${age}`)
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+console.log(name.indexOf('l'))
+
+let str = "      heyy-Neelam   "
+console.log(str.trim());
+console.log(str.replace("-", " "))
+console.log(str.includes("Heyy"));
+
+let score = "neelam:nidhi:nidhu"
+console.log(score.slice(-8,-2));
+console.log(score.split(':'));
