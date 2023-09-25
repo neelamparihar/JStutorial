f645e1acf2fbb3dbc51baa4697db01fbf03478c5
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-25 07:12:02 +0000

    message

---
+++ 02_basics/03_arrowfuc.js
@@ -0,0 +1,10 @@
+//this reference
+const chai = () =>  {
+   {username : "Neelam"}
+   console.log(this.username);
+}
+
+const addnum = (num1,num2) =>{
+    return num1+num2;
+}
+console.log(addnum(6,8));
---
+++ 02_basics/04_iife.js
@@ -0,0 +1,6 @@
+//Immediately invoked function expression
+(function chai(){
+    console.log("database connected");
+})()
+
+//If you want to write two IIFE then you have to end first IIFE by semicolon
